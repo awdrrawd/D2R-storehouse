c69b758dd3a897847af5b5d2bc46653a09b31ff1
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2026-03-24 20:48:31 +0800

    Update ui_traderie.js

--- d2r-translation-data/Platform/ui_traderie.js
+++ d2r-translation-data/Platform/ui_traderie.js
@@ -67,8 +67,9 @@ window.D2R_UI_TRADERIC = {
     "Mark Sold":         "已出售",
     "Relist":            "重新上架",
     "Remove":            "移除",
-    " hour(s)":          "小時",
-    " hour":             "小時",
+    " hour(s)":          "小時",//無效
+    " hour":             "小時",//無效
+    "Hide":              "隱藏",
     "Hide Listing":      "隱藏",
     "Share":             "分享",
     "Paste":             "貼上",
